7b5d6d2a3224cd185f123a3f14510da8a87a593b
Lee Hao Quan <kxngmadx@gmail.com>
2026-09-13 00:39:52 +0800

    Added the basic html structure and CSS styling

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-console.log('Hello world');
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error('fuck you');
+    }
+    return a / b;
+}
+console.log(divide(2, 0));
 export {};
 //# sourceMappingURL=script.js.map
